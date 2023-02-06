d4338ddc94ca7e3441a3be1cd246472b878ba56b
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-06 13:29:01 +0530

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,6 +1,6 @@
 const request = require('request');
  
-_EXTERNAL_URL = 'https://test-proj-heroku.herokuapp.com/api/plans';
+_EXTERNAL_URL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCLNZCmP_Wkf6mg_E_QjFLb8V5DlYdTbag&cx=018358168972005499115:qievzugb09r&q';
 
 const callExternalApiUsingRequest = (callback) => {
     request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
