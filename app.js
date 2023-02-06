136a3e42e974bff77faa55916b203f4016fe5367
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-06 09:59:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'); 
 var request = require('request'); 
 var app = express();
+const PORT = process.env.PORT || 3030;
 app.get('', function(req, res){ 
     request('https://www.googleapis.com/customsearch/v1?key=AIzaSyDV1joihnXLW7kB1V_rcYdJKBgvqjhZm4E&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans' , function (error, response, body) { 
       if (!error && response.statusCode === 200) { 
@@ -9,5 +10,6 @@ app.get('', function(req, res){
       } 
      }); 
   });
-app.listen(3000); 
-console.log('Server running on port %d', 3000);
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
