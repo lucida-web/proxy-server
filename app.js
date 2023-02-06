6d8aba8324b80a9f4471f9b6170b24367eb03362
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-06 12:53:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var request = require('request');
 var app = express();
 const PORT = process.env.PORT || 3030;
 app.get('', function(req, res){ 
-    request('https://www.googleapis.com/customsearch/v1?key=AIzaSyDV1joihnXLW7kB1V_rcYdJKBgvqjhZm4E&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans' , function (error, response, body) { 
+    request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCLNZCmP_Wkf6mg_E_QjFLb8V5DlYdTbag&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans' , function (error, response, body) { 
       if (!error && response.statusCode === 200) { 
         console.log(body); 
         res.send(body); 
