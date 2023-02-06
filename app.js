47e60dac26d39e36d5be3318a58ab83941309c8d
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-06 21:31:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,57 @@
 
-const apiCallFromRequest = require('./Request')
-const apiCallFromNode = require('./NodeJsCall')
+// const apiCallFromRequest = require('./Request')
+// const apiCallFromNode = require('./NodeJsCall')
 
-const http = require('http')
-// const PORT = process.env.PORT || 3030;
+// const http = require('http')
+// // const PORT = process.env.PORT || 3030;
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+// http.createServer((req, res) => {
+//         if(req.url === "/request"){
+//             apiCallFromRequest.callApi(function(response){
+//                 //console.log(JSON.stringify(response));
+//                 res.write(JSON.stringify(response));
+//                 res.end();
+//             });
+//         }
+//         else if(req.url === "/node"){
+//             apiCallFromNode.callApi(function(response){
+//                 res.write(response);
+//                 res.end();
+//             });
+//         }
+        
+//         // res.end();
+        
+// })
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-http.createServer((req, res) => {
-        if(req.url === "/request"){
-            apiCallFromRequest.callApi(function(response){
-                //console.log(JSON.stringify(response));
-                res.write(JSON.stringify(response));
-                res.end();
-            });
-        }
-        else if(req.url === "/node"){
-            apiCallFromNode.callApi(function(response){
-                res.write(response);
-                res.end();
-            });
-        }
-        
-        // res.end();
-        
-})
+
+app.get('/app', (req, res) => {
+  request(
+    { url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCsECJAADGD61ihtTGrCxynnr2hoQL87Bk&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-// =======
-// var express = require('express'); 
-// var request = require('request'); 
-// var app = express();
-// const PORT = process.env.PORT || 3030;
-// app.get('/', function(req, res){ 
-//     request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCLNZCmP_Wkf6mg_E_QjFLb8V5DlYdTbag&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans' , function (error, response, body) { 
-//       if (!error && response.statusCode === 200) { 
-//         console.log(body); 
-//         res.send(body); 
-//       } 
-//      }); 
-//   });
-// app.listen(PORT, () => {
-//   console.log(`server started on port ${PORT}`);
-// });
-// >>>>>>> c60e140c40263da6af05283c8735d8b38d9a14d8
