17ed2ca0774a97eebf09e91dab47e5a48a909c6a
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-06 13:42:10 +0530

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,6 +1,6 @@
 const request = require('request');
  
-_EXTERNAL_URL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCLNZCmP_Wkf6mg_E_QjFLb8V5DlYdTbag&cx=018358168972005499115:qievzugb09r&q';
+_EXTERNAL_URL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCsECJAADGD61ihtTGrCxynnr2hoQL87Bk&cx=018358168972005499115:qievzugb09r&q=children%20enjoy%20stories%20%20%20%20&start=1&searchType=image&imgSize=large&imgColorType=trans';
 
 const callExternalApiUsingRequest = (callback) => {
     request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
