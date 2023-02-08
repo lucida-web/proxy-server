296661dac5372dcc6d2750f67dd203f65c53de56
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-08 23:30:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,26 +26,27 @@
 // app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 
-const { default: axios } = require('axios');
 const express = require('express');
-const app = express();
- app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header ('Access-Control-Allow-Headers', 'Content-Type')
- next()
+const request = require('request');
 
-app.get("/", (req, res) => {
-//     axios.get('https://www.google.com/search&"&q=" +query')
-    axios.get('https://www.adaliaconfidenceandsuccessblog.com/wp-content/uploads/2014/04/rejection-quote.png')
-        .then(function (response) {
-            res.send(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/jokes/random', (req, res) => {
+  request(
+    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
 });
 
 const PORT = process.env.PORT || 3000;
