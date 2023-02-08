d1aed565abec9036b32799f425d83c9a9903baf3
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-08 20:07:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,10 +29,17 @@
 const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
+ app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header ('Access-Control-Allow-Headers', 'Content-Type')
+ next()
 
 app.get("/", (req, res) => {
 //     axios.get('https://www.google.com/search&"&q=" +query')
-    axios.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyCLNZCmP_Wkf6mg_E_QjFLb8V5DlYdTbag&cx=018358168972005499115:qievzugb09r&"&q=" +query')
+    axios.get('ttps://www.adaliaconfidenceandsuccessblog.com/wp-content/uploads/2014/04/rejection-quote.png')
         .then(function (response) {
             res.send(response.data);
         })
