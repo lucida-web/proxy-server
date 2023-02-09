075fe7aa4d0f37c1e5dd9c551609d2d32175faac
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 18:03:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
      res.header('Access-Control-Allow-Credentials', true)
      res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
      res.setheader ('Access-Control-Allow-Headers', 'Content-Type','image/html')
-     res.setHeader('Content-Type', 'text/html');
+     //res.setHeader('Content-Type', 'text/html');
      res.send(JSON.stringify(req.body));
      
   return next();
