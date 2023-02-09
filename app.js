6a2f3f546eeec34bbdc58cc2ec085adce4fbba4a
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 14:55:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 // });
 
 app.get('/', function(req, res){ 
-  request('https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json', function (error, response, body) { 
+  request('https://www.googleapis.com/customsearch/v1?q={searchTerms}&alt=json', function (error, response, body) { 
     if (!error && response.statusCode === 200) { 
       console.log(body); 
       res.send(body); 
