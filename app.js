964fd46391f404970bb1cda6ac722c3eae3bfb06
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 19:36:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,29 +15,38 @@ app.use((req, res, next) => {
      
   return next();
 });
-// String key="AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y";
-// String qry="queries";
-// String cx = "018358168972005499115:qievzugb09r";
-
-app.get('/', function(req, res){ 
-  request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCCBcF4NNB9nQcwXJogsXAudlNvlYO1-8o&cx=018358168972005499115:qievzugb09r&q=" +query +"&callback=hndlr', function (error, response, body) { 
-    if (!error && response.statusCode === 200) { 
-      console.log(body); 
-        res.send(body); 
-       //res.send(JSON.stringify(req.body));
-    } 
-   }); 
-});
 
 
-// app.get('/', function(req, res) {
-//     var url = proxiedURL +"?" + querystring.stringify(req.query);
-//     logger.info('/', URL); 
-//     fetch(url).then(actual => {
-//        actual.headers.forEach((v, n) => res.setHeader(n, v));
-//        actual.body.pipe(res);
-//     });
+// app.get('/', function(req, res){ 
+//   request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCCBcF4NNB9nQcwXJogsXAudlNvlYO1-8o&cx=018358168972005499115:qievzugb09r&q=" +query +"&callback=hndlr', function (error, response, body) { 
+//     if (!error && response.statusCode === 200) { 
+//       console.log(body); 
+//         res.send(body); 
+//        //res.send(JSON.stringify(req.body));
+//     } 
+//    }); 
 // });
+
+
+app.get('/', function(req, res) {
+const proxiedURL = 'https://proxy-server-s589.onrender.com'
+var url = proxiedURL + "?" + querystring.stringify(req.query);
+console.log('images url',url)
+
+logger.info('/api going to url', url);
+
+request.get(url, function(err, response, img) {
+    logger.info("response:", response.statusCode, response.headers['content-type']);
+    if (!err && response.statusCode === 200) {
+        res.writeHead(200, {
+            'Content-Type': response.headers['content-type']
+        });
+        // response.pipe(res);  // not working
+        res.writeHead(200, response.headers);
+        res.end(img, 'binary');
+    } else res.send("Error occurred:", err, "; status code: ", response.statusCode);
+})
+});
 
 
 
