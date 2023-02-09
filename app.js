1393557cfe9adee37987b2ed421ab1bb89dec915
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 16:35:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,34 +7,15 @@ app.use((req, res, next) => {
      res.header('Access-Control-Allow-Credentials', true)
      res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
      res.header ('Access-Control-Allow-Headers', 'Content-Type','application/x-www-form-urlencoded')
+     
   return next();
 });
 // String key="AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y";
 // String qry="queries";
 // String cx = "018358168972005499115:qievzugb09r";
 
-// app.get('/', function(req, res){ 
-//   request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&q=sun&alt=json&queriefields=queries(request(totalResults))"', function (error, response, body) { 
-//     if (!error && response.statusCode === 200) { 
-//       console.log(body); 
-//       res.send(body); 
-//     } 
-//    }); 
-// });
-
-// app.get('/', function(req, res){ 
-//   request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&q=sun&start=1&searchType=image&imgSize=large&imgColorType=trans', function (error, response, body) { 
-//     if (!error && response.statusCode === 200) { 
-//       console.log(body); 
-//       res.send(body); 
-//     } 
-//    }); 
-// });
-
-const apiKey ='AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y';
-const cx = '018358168972005499115:qievzugb09r';
 app.get('/', function(req, res){ 
-  request('https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${encodeURIComponent(query)}', function (error, response, body) { 
+  request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&q=" +query +"&callback=hndlr', function (error, response, body) { 
     if (!error && response.statusCode === 200) { 
       console.log(body); 
       res.send(body); 
@@ -42,14 +23,7 @@ app.get('/', function(req, res){
    }); 
 });
 
-// app.get('/', function(req, res){ 
-//   request('https://www.googleapis.com/customsearch/v1?q={searchTerms}&alt=json', function (error, response, body) { 
-//     if (!error && response.statusCode === 200) { 
-//       console.log(body); 
-//       res.send(body); 
-//     } 
-//    }); 
-// });
+
 
 
 const PORT = process.env.PORT || 3000;
