62703bf088781d664ef41ea2ab52b10dacd73f06
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 09:46:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,9 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
   }
   return next();
 });
