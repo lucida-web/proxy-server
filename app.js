84a91ac69339b27f0e332ad2ba2ccb93a2a42ccb
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 14:43:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ app.use((req, res, next) => {
   return next();
 });
 
+// app.get('/', function(req, res){ 
+//   request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&%22&q=%22%20+query%20+%22&callback=queryParamList', function (error, response, body) { 
+//     if (!error && response.statusCode === 200) { 
+//       console.log(body); 
+//       res.send(body); 
+//     } 
+//    }); 
+// });
+
 app.get('/', function(req, res){ 
-  request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&%22&q=%22%20+query%20+%22&callback=queryParamList', function (error, response, body) { 
+  request('https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json', function (error, response, body) { 
     if (!error && response.statusCode === 200) { 
       console.log(body); 
       res.send(body); 
