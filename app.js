aa4dac1bec61d6530a374596205e063e26bf36be
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 17:05:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const request = require('request');
 const app = express();
+var cors = require('cors')
+app.use(cors())
 app.use((req, res, next) => {
      res.setHeader('Access-Control-Allow-Origin', '*');
      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
