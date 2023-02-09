21277315901295c0db1b1f3049b1b76960627e8a
lucida-web <93707762+lucida-web@users.noreply.github.com>
2023-02-09 15:47:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 // String cx = "018358168972005499115:qievzugb09r";
 
 app.get('/', function(req, res){ 
-  request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&q="sun"&alt=json&queriefields=queries(request(totalResults))"', function (error, response, body) { 
+  request('https://www.googleapis.com/customsearch/v1?key=AIzaSyCpUz4HW_dBfwtOvnCzZsLaK0bncUOu78Y&cx=018358168972005499115:qievzugb09r&q=sun&alt=json&queriefields=queries(request(totalResults))"', function (error, response, body) { 
     if (!error && response.statusCode === 200) { 
       console.log(body); 
       res.send(body); 
